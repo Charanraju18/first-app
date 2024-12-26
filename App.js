c2c9e4e635086597183f1f3ed71d5b279eb0002f
Charanraju18 <charanraju925@gmail.com>
2024-12-26 21:11:42 +0530

    commit

--- App.js
+++ App.js
@@ -14,6 +14,7 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [carName, setCarName] = useState("Curvv");
+  const [select, setSelected] = useState("Curvv");
 
   const data = [
     {
@@ -85,6 +86,7 @@ const styles = StyleSheet.create({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
+    zIndex: 2,
   },
   carNameDiv: {
     width: 100,
