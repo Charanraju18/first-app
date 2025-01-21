170b8e7a949312b7a0ee843932ffdef51941aba0
Charanraju18 <charanraju925@gmail.com>
2025-01-21 12:07:35 +0530

    thub app commit

--- components/Toaster.js
+++ components/Toaster.js
@@ -1,28 +1,39 @@
 import Toast from "react-native-toast-message";
-import { Button, SafeAreaView, StatusBar, StyleSheet } from "react-native";
+import {
+  Button,
+  Image,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from "react-native";
+import { Text } from "react-native";
 import { useState } from "react";
+import img1 from "../assets/logo-dark.png";
+import img2 from "../assets/google.png";
+import img3 from "../assets/22MH1A4250.png";
 export default function Toaster() {
-  const [Show, setShow] = useState(false);
-  const ShowMsg = () => {
-    Toast.show({
-      type: "success",
-      text1: "Login failed",
-      text2: "You have successfully logged in",
-      position: "bottom",
-      visibilityTime: 2000,
-    });
-  };
-  //   const [bgColor, setbgColor] = useState("white");
-  //   const [textColor, settextColor] = useState("dark");
-  //   const changeStatusBar = () => {
-  //     settextColor(textColor === "dark" ? "light" : "dark");
-  //     setbgColor(bgColor === "white" ? "black" : "white");
-  //   };
+  const [username, setUsername] = useState("Charan Raju");
+  const icons = [
+    {
+      img: "",
+    },
+  ];
   return (
     <SafeAreaView style={mystyle.container}>
       <StatusBar backgroundColor={"white"} barStyle={"dark-content"} />
-      <Toast ref={(ref) => Toast.setRef(ref)} />
-      <Button mode="contained" onPress={ShowMsg} title="Click me" />
+      <View style={mystyle.mainView}>
+        <View style={mystyle.view1}>
+          <Image source={img1} style={mystyle.logo} />
+        </View>
+        <View>
+          <Text style={mystyle.welcome}>Welcome, {username}</Text>
+        </View>
+        <View style={mystyle.imageDiv}>{/* <Image source={img3} /> */}</View>
+        <View>
+          <Text style={mystyle.categories}>Categories</Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
@@ -30,13 +41,37 @@ export default function Toaster() {
 const mystyle = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "black",
+    backgroundColor: "white",
+  },
+  mainView: {
+    padding: 20,
+  },
+  view1: {
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
+    flexDirection: "row",
   },
-  btndiv: {
-    backgroundColor: "gray",
-    width: 200,
+  logo: {
+    height: 40,
+    width: 220,
+  },
+  welcome: {
+    top: 20,
+    fontWeight: 500,
+    fontSize: 20,
+    color: "green",
+  },
+  imageDiv: {
+    width: 393,
+    height: 250,
+    top: 40,
+    backgroundColor: "green",
+    borderRadius: 15,
+  },
+  categories: {
+    top: 60,
+    fontWeight: 500,
+    fontSize: 20,
+    color: "green",
   },
 });
