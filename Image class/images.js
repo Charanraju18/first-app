0eee57e949f66eacc39e5947b52883cbcec16c23
Charanraju18 <charanraju925@gmail.com>
2025-02-17 12:04:41 +0530

    opening camera also added

--- Image class/images.js
+++ Image class/images.js
@@ -1,18 +1,48 @@
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, Dimensions } from "react-native";
 import * as Picker from "expo-image-picker";
 import { Button } from "react-native-paper";
+import { useState } from "react";
 
 const ImagePicker = () => {
-  const pickImage = async () => {
-    const result = await Picker.getMediaLibraryPermissionsAsync();
-    console.log(result);
+  const { width, height } = Dimensions.get("screen");
+  const [image, setImage] = useState(null);
+
+  const showImagePicker = async () => {
+    const permissionResult = await Picker.requestMediaLibraryPermissionsAsync();
+    if (permissionResult.status === "granted") {
+      const result = await Picker.launchImageLibraryAsync();
+      setImage(result.assets[0].uri);
+    }
   };
 
+  const openCamera = async () => {
+    const permissionResult = await Picker.requestCameraPermissionsAsync();
+    if (permissionResult.status === "granted") {
+      const result = await Picker.launchCameraAsync();
+      setImage(result.assets[0].uri);
+    }
+  };
   return (
     <View style={styles.container}>
-      <Button onPress={pickImage} mode="contained">
-        Pick an image
-      </Button>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          width: width,
+          marginTop: 20,
+        }}
+      >
+        <Button mode="contained" onPress={showImagePicker}>
+          Pick Image
+        </Button>
+        <Button mode="contained" onPress={openCamera}>
+          Open Camera
+        </Button>
+      </View>
+      <View style={{ width: width, alignItems: "center" }}>
+        <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+      </View>
     </View>
   );
 };
@@ -20,8 +50,6 @@ const ImagePicker = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
