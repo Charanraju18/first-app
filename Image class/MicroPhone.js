f56839805a94764198c4878267797671188c3030
Charanraju18 <charanraju925@gmail.com>
2025-02-21 11:43:42 +0530

    Add microphone recording functionality

--- Image class/MicroPhone.js
+++ Image class/MicroPhone.js
@@ -1,10 +1,61 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Dimensions, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
 import { Audio } from "expo-av";
+import { Button } from "react-native-paper";
 export default function MicroPhone() {
+  const { width, height } = Dimensions.get("screen");
+  const [MyAudio, setMyAudio] = useState(null);
+  const [AudioPath, setAudioPath] = useState(null);
+
+  const Record = async () => {
+    const { status } = await Audio.requestPermissionsAsync();
+
+    if (status === "granted") {
+      const { recording } = await Audio.Recording.createAsync();
+      setMyAudio(recording);
+    }
+  };
+
+  const StopRecord = async () => {
+    if (MyAudio == null) return;
+    await MyAudio.stopAndUnloadAsync();
+    setAudioPath(MyAudio._uri);
+  };
+  const PlayAudio = async () => {
+    const { sound } = await Audio.Sound.createAsync({ uri: AudioPath });
+    await sound.playAsync();
+  };
+
   return (
-    <View>
-      <Text>MicroPhone</Text>
+    <View style={{ flex: 1, width: width, alignItems: "center" }}>
+      <View style={{ width: 250 }}>
+        <Button
+          mode="contained"
+          icon="microphone"
+          onPress={Record}
+          style={{ marginVertical: 10 }}
+        >
+          Record
+        </Button>
+
+        <Button
+          mode="contained"
+          icon="stop"
+          onPress={StopRecord}
+          style={{ marginVertical: 10 }}
+        >
+          Stop
+        </Button>
+
+        <Button
+          mode="contained"
+          icon="play"
+          onPress={PlayAudio}
+          style={{ marginVertical: 10 }}
+        >
+          Play
+        </Button>
+      </View>
     </View>
   );
 }
